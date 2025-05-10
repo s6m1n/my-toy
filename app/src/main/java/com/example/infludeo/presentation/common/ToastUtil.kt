1d3625c4b7e5018f9aca8a85af7a0009a49c786a
somin <salth0601@gmail.com>
2025-05-11 01:50:04 +0900

    feat: Toast 메시지 유틸 추가

---
+++ app/src/main/java/com/example/infludeo/presentation/common/ToastUtil.kt
@@ -0,0 +1,12 @@
+package com.example.infludeo.presentation.common
+
+import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
+
+fun showToast(
+    @StringRes id: Int,
+    context: Context,
+) {
+    Toast.makeText(context, id, Toast.LENGTH_SHORT).show()
+}

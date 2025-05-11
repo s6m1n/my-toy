5e20be34460d80e5e116b89b9417c6ae9be65a2b
somin <salth0601@gmail.com>
2025-05-11 21:34:26 +0900

    ui: TopBar 뒤로가기 버튼 구현

--- app/src/main/java/com/example/infludeo/presentation/RootScreen.kt
+++ app/src/main/java/com/example/infludeo/presentation/RootScreen.kt
@@ -1,16 +1,26 @@
 package com.example.infludeo.presentation
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.infludeo.R
 import com.example.infludeo.presentation.navigation.BottomNavBar
+import com.example.infludeo.presentation.navigation.NavScreen
 import com.example.infludeo.presentation.navigation.PokemonNavHost
 
 @Composable
@@ -25,12 +35,31 @@ fun RootScreen(appState: PokemonAppState) {
 
 @Composable
 private fun TopBar(appState: PokemonAppState) {
-    appState.currentScreen.value?.title?.let {
-        Text(
-            text = stringResource(it),
-            style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 24.sp),
-            modifier = Modifier.padding(start = 16.dp, top = 8.dp, bottom = 8.dp),
-        )
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        appState.currentScreen.value?.let { currentScreen ->
+            if (currentScreen != NavScreen.List && currentScreen != NavScreen.Favorite) {
+                Icon(
+                    painter = painterResource(R.drawable.icon_left_arrow),
+                    contentDescription = stringResource(R.string.back_arrow),
+                    modifier =
+                        Modifier
+                            .size(36.dp)
+                            .padding(start = 16.dp)
+                            .clickable(
+                                interactionSource = remember { MutableInteractionSource() },
+                                indication = null,
+                                onClick = { appState.moveToBackStack() },
+                            ),
+                )
+            }
+            Text(
+                text = stringResource(currentScreen.title),
+                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 24.sp),
+                modifier = Modifier.padding(start = 16.dp, top = 8.dp, bottom = 8.dp),
+            )
+        }
     }
 }
 

3dab329638956e832a700af0178322ec9c05bafb
somin <salth0601@gmail.com>
2025-06-14 14:39:56 +0900

    feat: 웹소켓을 위한 DI 모듈 구현

--- app/src/main/java/com/example/infludeo/di/RetrofitModule.kt
+++ app/src/main/java/com/example/infludeo/di/RetrofitModule.kt
@@ -1,11 +1,16 @@
 package com.example.infludeo.di
 
 import com.example.infludeo.data.remote.PokemonApiService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -13,5 +18,26 @@ import javax.inject.Singleton
 object RetrofitModule {
     @Singleton
     @Provides
+    fun provideRetrofit(
+        json: Json,
+        @Named("retrofit") okHttpClient: OkHttpClient,
+    ): Retrofit {
+        val contentType = CONTENT_TYPE.toMediaType()
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json { ignoreUnknownKeys = true }
+
+    @Singleton
+    @Provides
     fun providePokemonApiService(retrofit: Retrofit): PokemonApiService = retrofit.create(PokemonApiService::class.java)
+
+    private const val BASE_URL = "https://pokeapi.co/api/v2/"
+    private const val CONTENT_TYPE = "application/json"
 }

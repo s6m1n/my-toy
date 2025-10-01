06afc7ed3407de9ecf316303e63e77748a21c99e
somin <salth0601@gmail.com>
2025-10-01 22:21:43 +0900

    feat: 애플리케이션 전역 코루틴 스코프를 위한 모듈 추가

---
+++ app/src/main/java/com/example/bingtoy/di/CoroutineScopeModule.kt
@@ -0,0 +1,26 @@
+package com.example.bingtoy.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutineScopeModule {
+    @Provides
+    @Singleton
+    @ApplicationScope
+    fun provideApplicationScope(): CoroutineScope {
+        return CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    }
+}
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ApplicationScope
